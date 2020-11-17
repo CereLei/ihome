850a1959d3b5f9bdf256ebbe8603b320b958bc8e
zhaolei19 <zhaolei19@58.com>
2020-11-17 15:24:53 +0800

    迁移文件，日志

--- manage.py
+++ manage.py
@@ -1,15 +1,16 @@
 # 最基本的文件
-from ihome import create_app
+from ihome import create_app,db
+from flask_migrate import Migrate,MigrateCommand
+from flask_script import Manager
 
 
-
-
+# 创建flask的应用对象
 app = create_app('develop')
+manager = Manager(app)
 
-@app.route('/index')
-def index():
-    print('1')
-    return 'imde page'
+Migrate(app,db)
+manager.add_command("db",MigrateCommand)
+
 
 if __name__ == '__main__':
-    app.run()
+    manager.run()
